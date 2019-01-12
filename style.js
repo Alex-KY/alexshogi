7314ab2a7a0421414e0669eec17d5f2159798431
Алексей <kravchenko_ay@mail.ru>
2019-01-12 19:25:01 +0300

    update

---
+++ style.js
@@ -0,0 +1,25 @@
+const STYLE = `
+.pageBreak {
+    mso-special-character: line-break;
+    page-break-before: always;
+}
+p {
+    margin: 0;
+    margin-bottom: 1pt;
+}
+.font12pt * {
+    font-size: 12pt;
+}
+table.left td {
+    text-align: left;
+}
+.center {
+    text-align: center;
+}
+.center table {
+    margin: auto;
+}
+.right {
+    text-align: right;
+}
+`;
